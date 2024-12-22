2ae32f776490310c78a0b099e78730695c81408a
AbhayBS85 <abskunju@gmail.com>
2024-12-22 18:50:37 +0530

    app completed

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,10 +1,38 @@
 import React , {useState, useEffect} from 'react'
 
 function Clock() {
+
+    const [time,setTime] = useState(new Date());
+
+    useEffect(()=>{
+
+        document.title = `Time`;
+        const intervalId= setInterval(()=>{
+            setTime(new Date());
+        },1000);
+
+        return()=>{
+            clearInterval(intervalId);
+        }
+    },[])
+
+
+    function formateTime(){
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+
+        const meridian = hours >= 12 ? "PM" : "AM";
+
+        hours = hours % 12 || 12;
+
+        return `${hours} : ${minutes} : ${seconds} ${meridian}`;
+
+    }
   return (
     <div className='clock-container'>
         <div className='clock'>
-            <span>00:00:00</span>
+            <span>{formateTime()}</span>
         </div>
     </div>
   )
